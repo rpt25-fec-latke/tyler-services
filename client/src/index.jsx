8f10e7a4930bb26b5819611cfdb9c61b52b6fd29
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-01-13 20:44:00 -0800

    Ensure react transpiles to index.html

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  //code
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'));
