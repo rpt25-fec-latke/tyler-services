61ff7c1d8e1e2764395171cc26dad0b7f651a9dd
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-01-18 21:12:02 -0800

    Complete schema.sql script to create schema/tables
    Schema.sql script when run in the terminal creates 2 tables: users and reviews (joinable). Both of these will be where all the data lives for reviews.
    
    LAT-41

---
+++ schema.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS review_data;
+
+USE review_data;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  userName varchar(100) NOT NULL,
+  productsCount int NOT NULL,
+  reviewCount int NOT NULL,
+  profilePictureUrl varchar(255) NOT NULL,
+  hoursOnRecord float(10, 1) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+  id int NOT NULL AUTO_INCREMENT,
+  userId int NOT NULL,
+  gameId int NOT NULL,
+  reviewText varchar(255) NOT NULL,
+  isRecommended boolean NOT NULL,
+  commentCount int NOT NULL,
+  isHelpfulCount int NOT NULL,
+  isFunnyCount int NOT NULL,
+  isCleverAwardCount int NOT NULL,
+  warmBlanketAwardCount int NOT NULL,
+  saucyAwardCount int NOT NULL,
+  slowClapAwardCount int NOT NULL,
+  takeMyPointsAwardCount int NOT NULL,
+  wholesomeAwardCount int NOT NULL,
+  jesterAwardCount int NOT NULL,
+  fancyPantsAwardCount int NOT NULL,
+  whoaAwardCount int NOT NULL,
+  superStarAwardCount int NOT NULL,
+  wildAwardCount int NOT NULL,
+  treasureAwardCount int NOT NULL,
+  mindBlownAwardCount int NOT NULL,
+  goldenUnicornAwardCount int NOT NULL,
+  deepThoughtsAwardCount int NOT NULL,
+  heartWarmingAwardCount int NOT NULL,
+  hilariousAwardCount int NOT NULL,
+  hotTakeAwardCount int NOT NULL,
+  poetryAwardCount int NOT NULL,
+  extraHelpfulAwardCount int NOT NULL,
+  gottaHaveItAwardCount int NOT NULL,
+  michelangeloAwardCount int NOT NULL,
+  madScinetistAwardCount int NOT NULL,
+  isEarlyAccessReview boolean NOT NULL,
+  isPurchasedViaSteamKey boolean NOT NULL,
+  isActivatedViaSteamKey boolean NOT NULL,
+  userHoursOnRecordAtTimeOfReview float(10, 1) NOT NULL,
+  reviewLanguage varchar (50) NOT NULL,
+  reviewDate datetime NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userId) REFERENCES users (id)
+);
