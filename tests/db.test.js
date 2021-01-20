eb3c0f5a45d34dc9ecbc9f04d1dd2fcc210d383e
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-01-20 02:59:16 -0800

    Set up basic fake tests and set up dir structure
    Set up basic tests and exampel of testing React w/ Enzyme. Set up dir for test files and basic structure for db and API testing.
    
    LAT-54

---
+++ tests/db.test.js
@@ -0,0 +1,8 @@
+describe('Fake test', () => {
+  it('One should equal one', () => {
+    expect(1).toEqual(1);
+  });
+  it('Two should equal two', () => {
+    expect(2).toEqual(2);
+  })
+});
---
+++ tests/index.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from '../client/src/index.jsx';
+import { shallow } from 'enzyme';
+
+// describe('App component', () => {
+//   it('Contains a button with the text "Submit"', () => {
+//     let wrapper = shallow(<App />);
+//     let text = wrapper.find('button').text();
+//     expect(text).toEqual('Submit');
+//   });
+// });
+
+describe('Fake test', () => {
+  it('One should equal one', () => {
+    expect(1).toEqual(1);
+  });
+  it('Two should equal two', () => {
+    expect(2).toEqual(2);
+  })
+});
